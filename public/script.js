58129b27edf7c4e5685d9326483b885399c8bb40
Kyle Rosenstein <30937980+krosenstein@users.noreply.github.com>
2021-04-09 22:04:52 -0400

    Sequelize

--- public/script.js
+++ public/script.js
@@ -1,5 +1,58 @@
+let chart = new CanvasJS.Chart("chartContainer", {
+    title: {
+        text: "Meals and Macros"
+    },
+    axisX: {
+        valueFormatString: "DDD"
+    },
+    axisY: {
+        prefix: "$"
+    },
+    toolTip: {
+        shared: true
+    },
+    data: [{
+        type: "stackedBar",
+        name: "Meals",
+        showInLegend: "true",
+        dataPoints: [
+            { x: getMeal(), y: getMacro()}
+        ]
+    }]
+})
+
+async function getMeal() {
+    const endpoint = '/api/meals/';
+
+    const request = await fetch(endpoint);
+    const result = await request.json();
+
+    const meals = [];
+    meals.push(result.data);
+
+    randomNum = Math.random * meals.length;
+    return meals[random.num];
+}
+
+async function getMacro(meal) {
+    const endpoint = '/api/macros/';
+
+    const request = await fetch(endpoint);
+    const result = await request.json();
+
+    const macros = [];
+    macros.push(result.data);
+
+    macros.forEach((item) => {
+        if (item.meal_id == meal) {
+            return item;
+        }
+    })
+}
+
 async function getData() {
     const endpoint = '/api/dining/';
+
     const request = await fetch(endpoint);
     const result = await request.json();
 
